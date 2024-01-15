fa74724edc347aa4bc6d9f40517240fe36ea5103
Warlie Yap <137013560+warlie3d@users.noreply.github.com>
2024-01-15 14:50:34 -0500

    added get current location function

--- weather-check-app/src/WeatherApp.js
+++ weather-check-app/src/WeatherApp.js
@@ -1,5 +1,5 @@
 // src/components/WeatherApp.js
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import WeatherCard from './WeatherCard';
 import NavBar from './Components/NavBar';
@@ -10,6 +10,24 @@ import logo from "./images/weathercheck-logo-final.png"
 const WeatherApp = () => {
   const [zipCode, setZipCode] = useState('');
   const [weatherData, setWeatherData] = useState(null);
+
+  const getLocation = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        const latitude = position.coords.latitude;
+        const longitude = position.coords.longitude;
+        getWeatherDataByCoords(latitude, longitude);
+      }, (error) => {
+        console.error('Error getting geolocation', error)
+      })
+    } else {
+      console.error('Geolocation is not supported by this browser')
+    }
+  }
+
+  useEffect(() => {
+    //getLocation();
+  }, [])
 
   const handleZipCodeChange = (e) => {
     setZipCode(e.target.value);
@@ -26,6 +44,23 @@ const WeatherApp = () => {
     }
   };
 
+  //Function to get weather data using latitude and longitude
+  const getWeatherDataByCoords = async (latitude, longitude) => {
+    try {
+      const response = await axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=fd953eddf6f83f966d9240b60bd6fb26&units=imperial`
+      );
+      setWeatherData(response.data);
+    } catch (error) {
+      console.error('Error fetching weather data', error)
+    }
+  }
+
+  //Function to handle geolocation button click
+  const handleGeolocationClick = () => {
+    getLocation();
+  }
+
   return (
     <div className="appContainer">
       <div className="navbarContainer">
@@ -39,6 +74,8 @@ const WeatherApp = () => {
           onChange={handleZipCodeChange}
         />
         <button className="navbarItem" onClick={getWeatherData}>Get Weather</button>
+
+        <button className="navbarItem" onClick={handleGeolocationClick}>Use Current Location</button>
 
       <NavBar />
       </div>
